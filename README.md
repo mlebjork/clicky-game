e7da706332efe38ba57583e2a2c0a05adb942a25
Emelie Straus <mlestraus@gmail.com>
2019-01-24 20:13:29 -0700

    header and pic placeholders

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Check out out the game [here](https://mlebjork.github.io/clicky-game/.)This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const images =[
+  {title: 1, src: 'https://via.placeholder.com/150'},
+  {title: 2, src: 'https://via.placeholder.com/150'},
+  {title: 3, src: 'https://via.placeholder.com/150'},
+  {title: 4, src: 'https://via.placeholder.com/150'},
+  {title: 5, src: 'https://via.placeholder.com/150'},
+  {title: 6, src: 'https://via.placeholder.com/150'},
+  {title: 7, src: 'https://via.placeholder.com/150'},
+  {title: 8, src: 'https://via.placeholder.com/150'},
+  {title: 9, src: 'https://via.placeholder.com/150'},
+  {title: 10, src: 'https://via.placeholder.com/150'},
+  {title: 11, src: 'https://via.placeholder.com/150'},
+  {title: 12, src: 'https://via.placeholder.com/150'},
 
+]
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header>
+          <div><strong>Clicky game</strong></div>
+          <div>Click an image</div>
+          <div>Score: 0 | Top Score: 0</div>
+        </header>
+        <div>
+          {images.map((image)=>{
+            return  <img src={image.src} alt={image.title} onClick={
+              ()=>{
+                console.log(image)
+              }
+            }/>
+          })}
+        </div>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +47,7 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
       </div>
     );
   }
